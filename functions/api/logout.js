1e529a72e5a9ee046f03f8ae02dade25773d0949
Tantsa98 <tantsa98@gmail.com>
2026-08-18 11:54:27 +0300

    18.08.1

--- functions/api/logout.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  return new Response("OK", {
-    headers: {
-      "Set-Cookie": "auth=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0"
-    }
-  });
-}
